14f52acf8c88c5136eae0689d0a45de941a78656
2019wei <linkoko520@gmail.com>
2023-02-26 15:25:22 +0800

    i18n-fix

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const {createApp} = Vue;
 const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
 const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
 
 defineRule('required', required);
 defineRule('email', email);
 defineRule('min', min);
 defineRule('max', max);
+
+loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
+
+configure({
+  generateMessage: localize('zh_TW'),
+});
 
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'pororo03-api';
